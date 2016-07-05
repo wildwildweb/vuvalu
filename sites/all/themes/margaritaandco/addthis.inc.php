f3533b910ef656b7ca922c204f49adf0653d1825
Santiago Ervina <santiago.ervina@gmail.com>
2016-07-05 20:32:29 +0200

    Eliminar tema margaritaandco

--- sites/all/themes/margaritaandco/addthis.inc.php
+++
@@ -1,8 +0,0 @@
-<div class="addthis_toolbox addthis_16x16_style" style="left:50px;top:50px;">
-<a class="addthis_button_facebook">Facebook</a>
-<a class="addthis_button_twitter">Twitter</a>
-<a class="addthis_button_google_plusone_share">Google +</a>
-<a class="addthis_button_pinterest_share">Pinterest</a>
-</div>
-<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-51275e7a1de52cf4"></script>
