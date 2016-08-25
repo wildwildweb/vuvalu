48640b63d64a5dea5e3d193ebed6cebd09bd369b
Javi DC <javi@wildwildweb.es>
2016-08-25 16:21:05 +0200

    Añadido Flexslider y libreria. Menu Off Canvas. Cambios en CSS y plantillas. Nuevas imágenes.

--- sites/all/themes/vuvalu/region--menubar.tpl.php
+++ sites/all/themes/vuvalu/region--menubar.tpl.php
@@ -8,5 +8,6 @@
 <?php if ($content): ?>
   <nav id="<?php print $region; ?>" class="clearfix <?php print $classes; ?>">
     <?php print $content; ?>
+	<a href="#0" class="close-menu">Close<span></span></a> 
   </nav>
 <?php endif; ?>
